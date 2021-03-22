9ffbc52e38c9ec0c1dea7cf8e63c83750c10537f
ArtemMS <savenkoam@gmail.com>
2021-03-23 07:51:22 +0900

    Practice in List

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Snake
 {
@@ -11,6 +12,27 @@ namespace Snake
             
             Point p2 = new Point(4, 5, '#');            
             p2.Draw();
+
+            List<int> numList = new List<int>();
+            numList.Add(0);
+            numList.Add(1);
+            numList.Add(2);
+
+            int x = numList[0];
+            int y = numList[1];
+            int z = numList[2];
+
+            foreach (int item in numList)
+            {
+                Console.WriteLine(item);
+            }
+
+            numList.Remove(0);
+
+            List<Point> pList = new List<Point>();
+            pList.Add(p1);
+            pList.Add(p2);                        
+
         }
 
     }
