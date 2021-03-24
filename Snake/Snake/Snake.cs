3a03ab0f345f58fc54d3fa3594bd59bbe4712710
ArtemMS <savenkoam@gmail.com>
2021-03-24 12:55:42 +0900

    Create method Snake.Move

--- Snake/Snake/Snake.cs
+++ Snake/Snake/Snake.cs
@@ -1,20 +1,42 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Linq;
 
 namespace Snake
 {
     class Snake : Figure
     {
-        public Snake(Point tail, int lenght, Direction direction)
+        Direction direction;
+        public Snake(Point tail, int lenght, Direction _direction)
         {
+            direction = _direction;
             pList = new List<Point>();
             for (int i = 0; i < lenght; i++)
             {
                 Point p = new Point(tail);
-                p.Move(i, direction);
+                p.Move(i, _direction);
                 pList.Add(p);
             }
+        }
+
+        internal void Move()
+        {
+            Point tail = pList.First();
+            pList.Remove(tail);
+            Point head = GetNextPoint();
+            pList.Add(head);
+
+            tail.Clear();
+            head.Draw();
+        }
+
+        public Point GetNextPoint()
+        {
+            Point head = pList.Last();
+            Point nextPoint = new Point(head);
+            nextPoint.Move(1, direction);
+            return nextPoint;
         }
     }
 }
