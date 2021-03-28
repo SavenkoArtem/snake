673e9c6a8f78dec7b763821caf96e343ae3633ba
ArtemMS <savenkoam@gmail.com>
2021-03-28 13:34:04 +0900

    Create walls and funcrional crash

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -8,45 +8,10 @@ namespace Snake
     {
         static void Main(string[] args)
         {
-            VerticalLine v1 = new VerticalLine(0, 15, 5, '%');
-            Draw(v1);
+            Walls walls = new Walls(80, 25);
+            walls.Draw();
 
             Point p = new Point(4, 5, '*');
-            Figure fSnake = new Snake(p, 4, Direction.RIGHT);
-            Draw(fSnake);
-            Snake snake = (Snake)fSnake;
-
-            HorizontalLine h1 = new HorizontalLine(0, 5, 6, '&');
-
-            List<Figure> figures = new List<Figure>();
-            figures.Add(h1);
-            figures.Add(fSnake);
-            figures.Add(v1);
-            
-
-            foreach(var f in figures)
-            {
-                f.Draw();
-            }
-
-
-
-
-            
-            /*Console.SetBufferSize(width: 120, height: 120);
-
-            // Drawing a frame
-            HorizontalLine upLine = new HorizontalLine(0, 78, 0, '+');
-            HorizontalLine downLine = new HorizontalLine(0, 78, 24, '+');
-            VerticalLine leftLine = new VerticalLine(0, 24, 0, '+');
-            VerticalLine rightLine = new VerticalLine(0, 24, 78, '+');
-            upLine.Draw();
-            downLine.Draw();
-            leftLine.Draw();
-            rightLine.Draw();
-
-            // Drawing points
-            Point p = new Point(10, 15, '*');
             Snake snake = new Snake(p, 4, Direction.RIGHT);
             snake.Draw();
 
@@ -54,10 +19,12 @@ namespace Snake
             Point food = foodCreator.CreateFood();
             food.Draw();
 
-
-
             while (true)
             {
+                if (walls.IsHit(snake) || snake.IsHitTail())
+                {
+                    break;
+                }
                 if (snake.Eat(food))
                 {
                     food = foodCreator.CreateFood();
@@ -69,15 +36,13 @@ namespace Snake
                 }
 
                 Thread.Sleep(100);
-
                 if (Console.KeyAvailable)
                 {
                     ConsoleKeyInfo key = Console.ReadKey();
                     snake.HandleKey(key.Key);
-                }                         
-            }        */   
-            
-            
+                }
+
+            }
 
         }  
         
